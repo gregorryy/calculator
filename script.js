350077de24c5a82433bcff0bc446dec9e61245fe
greg <g.coraboeuf84@gmail.com>
2024-11-11 15:32:33 +0100

    Not finish but its okay

--- script.js
+++ script.js
@@ -1,143 +1,165 @@
-let number = 1;
-let operator = "+";
-let number2 = 2;
+let displayValue = "0";
+let currentOperator = null;
+let firstOperand = null;
 
-//ajouter
-function add(n1, n2) {
-  return n1 + n2;
+function clearDisplay() {
+  displayValue = "";
+  currentOperator = null;
+  firstOperand = null;
+  updateDisplay();
 }
-//console.log(add(1, 2)); // affiche 3
 
-//soustraire
-function substarct(n1, n2) {
-  return n1 - n2;
+function appendNumber(number) {
+  displayValue += number;
+  updateDisplay();
 }
-//console.log(substarct(1, 2)); // affiche -1
 
-//multiplier
-function multiply(n1, n2) {
-  return n1 * n2;
-}
-//console.log(multiply(1, 2)); // affiche 2
-
-//diviser
-function divide(n1, n2) {
-  return n1 / n2;
-}
-//console.log(divide(1, 2)); // affiche 0.5
-
-function operate() {
-  if (operator === "+") {
-    return add(number, number2);
-  } else if (operator === "-") {
-    return substarct(number, number2);
-  } else if (operator === "*") {
-    return multiply(number, number2);
-  } else if (operator === "/") {
-    return divide(number, number2);
+function appendOperator(operator) {
+  if (firstOperand === null && displayValue !== "") {
+    firstOperand = parseFloat(displayValue);
+    currentOperator = operator;
+    displayValue = "";
   }
 }
 
-console.log(operate());
+function deleteLastChar() {
+  let value = (displayValue.textContent = displayValue.slice(0, -1));
+  displayValue = value;
+  updateDisplay();
+}
+
+function disableButtons() {
+  document.getElementById("point").disabled = true;
+}
+
+function enableButtons() {
+  document.getElementById("point").disabled = false;
+}
+
+function calculate() {
+  if (currentOperator !== null && displayValue !== "") {
+    const secondOperand = parseFloat(displayValue);
+    let result = 0;
+
+    switch (currentOperator) {
+      case "+":
+        result = firstOperand + secondOperand;
+        break;
+      case "-":
+        result = firstOperand - secondOperand;
+        break;
+      case "*":
+        result = firstOperand * secondOperand;
+        break;
+      case "/":
+        result = firstOperand / secondOperand;
+        break;
+    }
+
+    displayValue = result.toString();
+    currentOperator = null;
+    firstOperand = null;
+    updateDisplay();
+  }
+}
+
+if (appendOperator("/") && appendNumber("0")) {
+  console.log("test");
+}
+
+function updateDisplay() {
+  const display = document.getElementById("display");
+  display.value = displayValue;
+}
 
 const SEPTbtn = document.querySelector("#sept");
 SEPTbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "7";
+  appendNumber("7");
 });
 
 const HUITbtn = document.querySelector("#huit");
 HUITbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "8";
+  appendNumber("8");
 });
 
 const NEUFbtn = document.querySelector("#neuf");
 NEUFbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "9";
+  appendNumber("9");
 });
 
 const QUATREbtn = document.querySelector("#quatre");
 QUATREbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "4";
+  appendNumber("4");
 });
 
 const CINQbtn = document.querySelector("#cinq");
 CINQbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "5";
+  appendNumber("5");
 });
 
 const SIXbtn = document.querySelector("#six");
 SIXbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "6";
+  appendNumber("6");
 });
 
 const TROISbtn = document.querySelector("#trois");
 TROISbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "3";
+  appendNumber("3");
 });
 
 const DEUXbtn = document.querySelector("#deux");
 DEUXbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "2";
+  appendNumber("2");
 });
 
 const UNbtn = document.querySelector("#un");
 UNbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "1";
+  appendNumber("1");
 });
 
 const ZIZOUbtn = document.querySelector("#zéro");
 ZIZOUbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "0";
+  appendNumber("0");
 });
 
 const ptnbtn = document.querySelector("#point");
 ptnbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = ".";
+  appendNumber(".");
+  disableButtons();
 });
 
 const CEbtn = document.querySelector("#CE");
 CEbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "0";
+  clearDisplay();
+  enableButtons();
 });
 
 const divbtn = document.querySelector("#÷");
 divbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "÷";
+  appendOperator("/");
 });
 
 const mulbtn = document.querySelector("#x");
 mulbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "x";
+  appendOperator("*");
 });
 
 const subbtn = document.querySelector("#moins");
 subbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "-";
+  appendOperator("-");
 });
 
 const plusbtn = document.querySelector("#plus");
 plusbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = "+";
+  appendOperator("+");
 });
 
 const Equbtn = document.querySelector("#égal");
 Equbtn.addEventListener("click", () => {
-  const screen = document.querySelector(".screen");
-  screen.textContent = add(number, number2);
+  calculate();
+});
+
+const delbt = document.querySelector("#del");
+delbt.addEventListener("click", () => {
+  deleteLastChar();
 });
