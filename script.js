01a9d4f363fb70b80a321c3b53408384c04891ee
greg <g.coraboeuf84@gmail.com>
2024-11-13 14:19:30 +0100

    Better UI steel not perfect we keep going

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-let displayValue = "0";
+let displayValue = "";
 let currentOperator = null;
 let firstOperand = null;
+//let functionEnabled = "non";
 
 function clearDisplay() {
   displayValue = "";
@@ -28,15 +29,56 @@ function deleteLastChar() {
   updateDisplay();
 }
 
-function disableButtons() {
+function disablePButtons() {
   document.getElementById("point").disabled = true;
 }
 
-function enableButtons() {
+function enablePButtons() {
   document.getElementById("point").disabled = false;
 }
 
+function disableButtons() {
+  document.getElementById("sept").disabled = true;
+  document.getElementById("huit").disabled = true;
+  document.getElementById("neuf").disabled = true;
+  document.getElementById("quatre").disabled = true;
+  document.getElementById("cinq").disabled = true;
+  document.getElementById("six").disabled = true;
+  document.getElementById("un").disabled = true;
+  document.getElementById("deux").disabled = true;
+  document.getElementById("trois").disabled = true;
+  document.getElementById("zéro").disabled = true;
+  document.getElementById("égal").disabled = true;
+  document.getElementById("÷").disabled = true;
+  document.getElementById("plus").disabled = true;
+  document.getElementById("moins").disabled = true;
+  document.getElementById("del").disabled = true;
+  document.getElementById("point").disabled = true;
+  document.getElementById("x").disabled = true;
+}
+
+function enableButtons() {
+  document.getElementById("sept").disabled = false;
+  document.getElementById("huit").disabled = false;
+  document.getElementById("neuf").disabled = false;
+  document.getElementById("quatre").disabled = false;
+  document.getElementById("cinq").disabled = false;
+  document.getElementById("six").disabled = false;
+  document.getElementById("un").disabled = false;
+  document.getElementById("deux").disabled = false;
+  document.getElementById("trois").disabled = false;
+  document.getElementById("zéro").disabled = false;
+  document.getElementById("égal").disabled = false;
+  document.getElementById("÷").disabled = false;
+  document.getElementById("plus").disabled = false;
+  document.getElementById("moins").disabled = false;
+  document.getElementById("del").disabled = false;
+  document.getElementById("point").disabled = false;
+  document.getElementById("x").disabled = false;
+}
+
 function calculate() {
+  //functionEnabled = "oui";
   if (currentOperator !== null && displayValue !== "") {
     const secondOperand = parseFloat(displayValue);
     let result = 0;
@@ -62,6 +104,16 @@ function calculate() {
     updateDisplay();
   }
 }
+
+//document.addEventListener("click", (event) => {
+//if (event.target.tagName === "BUTTON") {
+//console.log(functionEnabled);
+//}
+// });
+
+//if ((functionEnabled = "oui")) {
+//console.log("oui");
+//}
 
 if (appendOperator("/") && appendNumber("0")) {
   console.log("test");
@@ -125,13 +177,14 @@ ZIZOUbtn.addEventListener("click", () => {
 const ptnbtn = document.querySelector("#point");
 ptnbtn.addEventListener("click", () => {
   appendNumber(".");
-  disableButtons();
+  disablePButtons();
 });
 
 const CEbtn = document.querySelector("#CE");
 CEbtn.addEventListener("click", () => {
   clearDisplay();
   enableButtons();
+  enablePButtons();
 });
 
 const divbtn = document.querySelector("#÷");
@@ -156,6 +209,8 @@ plusbtn.addEventListener("click", () => {
 
 const Equbtn = document.querySelector("#égal");
 Equbtn.addEventListener("click", () => {
+  disableButtons();
+  console.log(disableButtons());
   calculate();
 });
 
